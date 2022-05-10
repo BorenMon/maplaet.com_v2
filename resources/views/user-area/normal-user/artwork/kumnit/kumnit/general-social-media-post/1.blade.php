a23ba253e9e8f73795fb13b3b3276124bc8e6356
Mon Boren <borenmon5@gmail.com>
2022-05-11 00:59:55 +0700

    Update 1.blade.php

--- resources/views/user-area/normal-user/artwork/kumnit/kumnit/general-social-media-post/1.blade.php
+++ resources/views/user-area/normal-user/artwork/kumnit/kumnit/general-social-media-post/1.blade.php
@@ -461,6 +461,30 @@
 
 @section('js')
 <script>
+  // ***Here is the code for converting "image source" (url) to "Base64".***
+
+let url = 'https://cdn.shopify.com/s/files/1/0234/8017/2591/products/young-man-in-bright-fashion_925x_f7029e2b-80f0-4a40-a87b-834b9a283c39.jpg'
+const toDataURL = url => fetch(url)
+      .then(response => response.blob())
+      .then(blob => new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = () => resolve(reader.result)
+      reader.onerror = reject
+      reader.readAsDataURL(blob)
+     }))
+
+
+// ***Here is code for converting "Base64" to javascript "File Object".***
+
+  function dataURLtoFile(dataurl, filename) {
+     let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+     bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+     while(n--){
+     u8arr[n] = bstr.charCodeAt(n);
+     }
+   return new File([u8arr], filename, {type:mime});
+  }
+
   // Upload Image Into Artwork Handler
   const inputImageURL = url => {
     const tester = new Image()
@@ -470,11 +494,27 @@
     }
     tester.onload = () => {
       destroyCropper()
-      $('.featured-image').each(function() {
-        featuredImgSrc = url
-        this.src = featuredImgSrc
+      toDataURL(url)
+      .then(dataUrl => {
+        const file = dataURLtoFile(dataUrl, "imageName.jpg")
+        new Compressor(file, {
+            quality : 0.8,
+            maxHeight: 2000,
+            maxWidth: 2000,
+            success(result) {
+              $('.featured-image').each((i, obj) => {
+                obj.src = URL.createObjectURL(result)
+              })
+              $('#cropperImg').attr('src', URL.createObjectURL(result))
+            }
+          }
+        )
       })
-      $('#cropperImg').attr('src', featuredImgSrc)
+      // $('.featured-image').each(function() {
+      //   featuredImgSrc = url
+      //   this.src = featuredImgSrc
+      // })
+      // $('#cropperImg').attr('src', featuredImgSrc)
       closeUploadModal()
     }
   }
@@ -500,11 +540,23 @@
     const file = e.dataTransfer.files[0]
     if(file.type.includes('image')){
     destroyCropper()
-    $('.featured-image').each(function() {
-      featuredImgSrc = URL.createObjectURL(file)
-      this.src = featuredImgSrc
-    })
-    $('#cropperImg').attr('src', featuredImgSrc)
+    new Compressor(file, {
+        quality : 0.8,
+        maxHeight: 2000,
+        maxWidth: 2000,
+        success(result) {
+          $('.featured-image').each((i, obj) => {
+            obj.src = URL.createObjectURL(result)
+          })
+          $('#cropperImg').attr('src', URL.createObjectURL(result))
+        }
+      }
+    )
+    // $('.featured-image').each(function() {
+    //   featuredImgSrc = URL.createObjectURL(file)
+    //   this.src = featuredImgSrc
+    // })
+    // $('#cropperImg').attr('src', featuredImgSrc)
     closeUploadModal()
     }
   })
