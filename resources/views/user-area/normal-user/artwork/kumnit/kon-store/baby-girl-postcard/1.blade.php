420ce82c57501f912821f23d21177bab865cbb1c
Mon Boren <borenmon5@gmail.com>
2022-05-21 09:13:59 +0700

    *

--- resources/views/user-area/normal-user/artwork/kumnit/kon-store/baby-girl-postcard/1.blade.php
+++ resources/views/user-area/normal-user/artwork/kumnit/kon-store/baby-girl-postcard/1.blade.php
@@ -5,131 +5,14 @@
 
 @section('css')
   @include('layouts.normal-user.default-artwork-css')
-  <style>
-    @font-face {
-      font-family: 'Kh-Ang-Midnight';
-      src: url('/assets/kumnit/fonts/Kh-Ang-Midnight.otf');
-    }
-    @font-face {
-      font-family: 'FredokaOne';
-      src: url('/assets/kumnit/fonts/FredokaOne-Regular.ttf');
-    }
-    @font-face {
-      font-family: 'AKbalthom-Freehand';
-      src: url('/assets/kumnit/fonts/AKbalthom-Freehand.ttf');
-    }
-    .artwork-preview {
-      width: 88vw;
-      height: calc(352vw / 5);
-      position: relative;
-      overflow: hidden;
-      background-color: white;
-      .featured-image {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        object-position: center;
-      }
-      .wish {
-        @apply absolute top-1/2 left-0 w-full flex flex-col items-center;
-
-        .main {
-          min-width: 70%;
-          max-width: 100%;
-          overflow: hidden;
-          background-color: white;
-          text-align: center;
-          white-space: nowrap;
-          font-family: 'FredokaOne', 'Kh-Ang-Midnight', sans-serif;
-          color: #34c8f4;
-          padding: 1vw 2vw;
-          border-radius: 2vw;
-          font-size: 4vw;
-        }
-        .secondary {
-          color: white;
-          font-family: 'AKbalthom-Freehand', sans-serif;
-          text-align: center;
-          width: 80%;
-          margin-top: 1.5vw;
-          font-size: 2.5vw;
-          word-wrap: break-word;
-        }
-      }
-      .from {
-        position: absolute;
-        bottom: 3%;
-        left: 0;
-        width: 100%;
-        text-align: center;
-        color: white;
-        font-family: 'AKbalthom-Freehand', sans-serif;
-        overflow: hidden;
-        white-space: nowrap;
-        font-size: 2.5vw;
-      }
-    }
-
-    #download {
-      position: fixed;
-      top: 0;
-      left: 0;
-      z-index: -1;
-      width: calc(100vw * 7);
-      height: calc(990vw * 7/7);
-      background-color: white;
-      padding: calc(7vw * 7);
-
-      .artwork-preview {
-        width: calc(45vw * 7);
-        height: calc(36vw * 7);
-        .wish {   
-          .main {
-            padding: calc(1vw * 45 / 88 * 7) calc(2vw * 45 / 88 * 7);
-            border-radius: calc(2vw * 45 / 88 * 7);
-            font-size: calc(4vw * 45 / 88 * 7);
-          }
-          .secondary {
-            margin-top: calc(1.5vw * 45 / 88 * 7);
-            font-size: calc(2.5vw * 45 / 88 * 7);
-          }
-        }
-        .from {
-          font-size: calc(2.5vw * 45 / 88 * 7);
-        }
-      }
-    }
-
-    @media(min-width: 800px) {
-      .customized-container {
-        .artwork-preview {
-          width: 36vw;
-          height: calc(144vw / 5);
-          .wish {   
-            .main {
-              padding: calc(1vw * 36 / 88) calc(2vw * 36 / 88);
-              border-radius: calc(2vw * 36 / 88);
-              font-size: calc(4vw * 36 / 88);
-            }
-            .secondary {
-              margin-top: calc(1.5vw * 36 / 88);
-              font-size: calc(2.5vw * 36 / 88);
-            }
-          }
-          .from {
-            font-size: calc(2.5vw * 36 / 88);
-          }
-        }
-      }
-    }
-  </style>
+  <link rel="stylesheet" href="{{ asset('css/production.css') }}">
 @endsection
 
 @section('content')
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
   <div id="download">
     <div class="artwork-preview">
-      <img src="{{ asset('assets/kumnit/images/kon-store/baby-boy-postcard/1/background.svg') }}" alt="" class="featured-image">
+      <img src="{{ asset('assets/kumnit/images/kon-store/baby-girl-postcard/1/background.svg') }}" alt="" class="featured-image">
       <div class="wish">
         <div class="main hidden"></div>
         <div class="secondary hidden"></div>
@@ -141,7 +24,7 @@
 
   <div class="customized-container">
     <div class="artwork-preview shadow">
-      <img src="{{ asset('assets/kumnit/images/kon-store/baby-boy-postcard/1/background.svg') }}" alt="" class="featured-image">
+      <img src="{{ asset('assets/kumnit/images/kon-store/baby-girl-postcard/1/background.svg') }}" alt="" class="featured-image">
       <div class="wish">
         <div class="main hidden"></div>
         <div class="secondary hidden"></div>
