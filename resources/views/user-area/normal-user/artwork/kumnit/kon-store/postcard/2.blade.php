148de0fff2f53546174093985e6ebdf840f246cc
Mon Boren <borenmon5@gmail.com>
2022-05-21 23:50:53 +0700

    *

--- resources/views/user-area/normal-user/artwork/kumnit/kon-store/postcard/2.blade.php
+++ resources/views/user-area/normal-user/artwork/kumnit/kon-store/postcard/2.blade.php
@@ -325,10 +325,18 @@
       })
       .then(dataUrl => {
         $('#loading').css('display', 'none')
-        var link = document.createElement('a')
-        link.download = 'poster.jpeg'
-        link.href = dataUrl
-        link.click()
+        new Compressor(dataURLtoFile(dataUrl), {
+            quality : 0.8,
+            maxHeight: 2000,
+            maxWidth: 2000,
+            success(result) {
+              const link = document.createElement('a')
+              link.download = `${fileName}.jpeg`
+              link.href = URL.createObjectURL(result)
+              link.click()
+            }
+          }
+        )
       })
     })
   })
